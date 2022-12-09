817748a3b9f1ad3be159430ca00380c6021a8fb2
Jade <jades@pop-os.localdomain>
2022-12-09 14:27:27 -0300

    mudanças para corrigir erro do swagger

--- client/src/main/java/com/example/demo/configs/SwaggerConfiguration.java
+++ client/src/main/java/com/example/demo/configs/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -26,8 +27,8 @@ public class SwaggerConfiguration {
                 .build()
                 .apiInfo(infosApi());
     }
-
-    private ApiInfo infosApi() {
+//montei em dois formatos que aprendi, para ver se funciona, mas não...
+    private ApiInfo infosApi2() {
 //michelle exemplo
         ApiInfo apiInfo = new ApiInfo(
                 "Clinic - API Documentation ",
@@ -39,10 +40,19 @@ public class SwaggerConfiguration {
                 "Apache License Version 2.0",
                 "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
         );
-    //panela dev exemplo
 
 
         return apiInfo;
+    }
+
+    //panela dev exemplo
+    private ApiInfo infosApi() {
+        return new ApiInfoBuilder()
+                .title("Spring API - Clinic")
+                .description("API para gerenciamento de clínicas.")
+                .version("1.0.0")
+                .contact(new Contact("Jade Santos", "http://github.com/devgirlpower", "jade@email"))
+                .build();
     }
 
 }
